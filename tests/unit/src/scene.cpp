3cf9dc071aa5f6fb916c5a391c3674dc5b1073bb
Ivan Gagis <igagis@gmail.com>
2025-10-12 13:03:07 +0300

    fix memory problems in unit tests due to parallel execution

--- tests/unit/src/scene.cpp
+++ tests/unit/src/scene.cpp
@@ -7,40 +7,64 @@
 
 namespace {
 const tst::set set("scene", [](tst::suite& suite) {
-	suite.add("basic_read", []() {
-		auto rc = utki::make_shared<ruis::render::null::context>();
-		{
-			ruis::render::gltf_loader l(rc.get());
-			auto scene = l.load(papki::fs_file("samples_gltf/kub.glb"));
-			tst::check(!scene.get().nodes.empty(), SL);
+	suite.add(
+		"basic_read", //
+		// test cannot be run in parallel with other tests using ruis::render::context
+		// because of the global current context stack in ruis::render::context.
+		tst::flag::no_parallel,
+		[]() {
+			auto rc = utki::make_shared<ruis::render::null::context>();
+			{
+				ruis::render::gltf_loader l(rc.get());
+				auto scene = l.load(papki::fs_file("samples_gltf/kub.glb"));
+				tst::check(!scene.get().nodes.empty(), SL);
+			}
 		}
-	});
+	);
 
-	suite.add("hierarchy", []() {
-		auto rc = utki::make_shared<ruis::render::null::context>();
-		{
-			ruis::render::gltf_loader l(rc.get());
-			auto scene = l.load(papki::fs_file("samples_gltf/parent_and_children.glb"));
-			tst::check(!scene.get().nodes[0].get().children.empty(), SL);
+	suite.add(
+		"hierarchy", //
+		// test cannot be run in parallel with other tests using ruis::render::context
+		// because of the global current context stack in ruis::render::context.
+		tst::flag::no_parallel,
+		[]() {
+			auto rc = utki::make_shared<ruis::render::null::context>();
+			{
+				ruis::render::gltf_loader l(rc.get());
+				auto scene = l.load(papki::fs_file("samples_gltf/parent_and_children.glb"));
+				tst::check(!scene.get().nodes[0].get().children.empty(), SL);
+			}
 		}
-	});
+	);
 
-	suite.add("spray_paint_model", []() {
-		auto rc = utki::make_shared<ruis::render::null::context>();
-		{
-			ruis::render::gltf_loader l(rc.get());
-			auto scene = l.load(papki::fs_file("samples_gltf/spray.glb"));
-			tst::check(!scene.get().nodes.empty(), SL);
+	suite.add(
+		"spray_paint_model", //
+		// test cannot be run in parallel with other tests using ruis::render::context
+		// because of the global current context stack in ruis::render::context.
+		tst::flag::no_parallel,
+		[]() {
+			auto rc = utki::make_shared<ruis::render::null::context>();
+			{
+				ruis::render::gltf_loader l(rc.get());
+				auto scene = l.load(papki::fs_file("samples_gltf/spray.glb"));
+				tst::check(!scene.get().nodes.empty(), SL);
+			}
 		}
-	});
+	);
 
-	suite.add("vintage_camera_model", []() {
-		auto rc = utki::make_shared<ruis::render::null::context>();
-		{
-			ruis::render::gltf_loader l(rc.get());
-			auto scene = l.load(papki::fs_file("samples_gltf/camera.glb"));
-			tst::check(!scene.get().nodes.empty(), SL);
+	suite.add(
+		"vintage_camera_model", //
+		// test cannot be run in parallel with other tests using ruis::render::context
+		// because of the global current context stack in ruis::render::context.
+		tst::flag::no_parallel,
+		[]() {
+			auto rc = utki::make_shared<ruis::render::null::context>();
+			{
+				ruis::render::gltf_loader l(rc.get());
+				auto scene = l.load(papki::fs_file("samples_gltf/camera.glb"));
+				tst::check(!scene.get().nodes.empty(), SL);
+			}
 		}
-	});
+	);
 });
 } // namespace
